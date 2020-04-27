ef6be05d5bd12445264367212882e41cddd313af
rendaletaas <36393469+rendaletaas@users.noreply.github.com>
2020-04-26 19:05:35 -0700

    Add files via upload

--- perm.sv
+++ perm.sv
@@ -1,3 +1,14 @@
+//LEVEL 0 PERM
+// LEVEL 1 INPUT_INTERFACE
+// LEVEL 1 PERM_LOGIC
+//  LEVEL 2 PERM_LAYER
+//   LEVEL 3 THETA
+//   LEVEL 3 RHO
+//   LEVEL 3 PI
+//   LEVEL 3 CHI
+//   LEVEL 3 IOTA
+// LEVEL 1 OUTPUT_INTERFACE
+
 `define empty 2'b00
 `define firstpush 2'b01
 `define full 2'b11
@@ -28,9 +39,9 @@ wire endofstring, endoflogic;
 inputInterface input1 (clk, reset, dix, din, pushin, data0);
 permLogic logic1 (clk, reset, endofstring, data0, endoflogic, data1);
 
-assign endofstring = &din && pushin;
+assign endofstring = &dix && pushin;
 
-assign debugtap = data0;
+assign debugtap = data1;
 
 endmodule  //end perm
 
@@ -136,7 +147,10 @@ end
 endmodule
 /*============ END INPUT_INTERFACE ============*/
 
+
+
 /*============ LEVEL 1 PERM_LOGIC ============*/
+
 module permLogic (clk, reset, pushin, din, pushout, dout);
 
 input                   clk, reset, pushin;
@@ -149,20 +163,245 @@ wire [4:0][4:0][63:0]   din;
 wire                    pushout;
 wire [4:0][4:0][63:0]   dout;
 
+wire [4:0][4:0][63:0]   data0, data1, data2;
+
 /*======== REGISTER UPDATE ========*/
 reg [1:0]               state, nextstate;
 reg [4:0][4:0][63:0]    recirculate;
+reg [6:0]               ringcounter;
+reg [31:0]              datatag;
 always @ (posedge clk) begin
     state <= nextstate;
     if ((state == `firstpush) || (state == `full)) begin
         recirculate <= dout;
     end
-    
+    if (state == `firstpush) begin
+        datatag[31:28] <= din[4][4][63:60];
+        datatag[27:24] <= din[1][4][55:52];
+        datatag[23:20] <= din[3][3][47:44];
+        datatag[19:16] <= din[0][3][39:36];
+        datatag[15:12] <= din[2][2][31:28];
+        datatag[11:8] <=  din[4][1][23:20];
+        datatag[7:4] <=   din[1][1][15:12];
+        datatag[3:0] <=   din[3][0][7:4];
+    end
+    if (state == `full) begin
+        ringcounter[0] <= ringcounter[6];
+        ringcounter[1] <= ringcounter[0];
+        ringcounter[2] <= ringcounter[1];
+        ringcounter[3] <= ringcounter[2];
+        ringcounter[4] <= ringcounter[3];
+        ringcounter[5] <= ringcounter[4];
+        ringcounter[6] <= ringcounter[5];
+    end
 end
 /*==== END REGISTER UPDATE ====*/
 
+/*======== RESET ========*/
+integer i;
+always @ (negedge reset) begin
+    state <= `restart ;
+    recirculate <= 200'b0;
+    datatag <= 32'b0;
+    for(i=1;i<7;i=i+1) begin
+        ringcounter[i] <= 1'b0;
+    end
+    ringcounter[0] <= 1'b1;
+end
+/*==== END RESET ====*/
+
 /*======== STATE MACHINE ========*/
+always @ (*) begin
+    case (state)
+        `empty : begin
+            if (pushin) begin
+                nextstate = `firstpush;
+            end
+            else begin
+                nextstate = `empty;
+            end
+        end  //end empty
+
+        `firstpush : begin
+            nextstate = `full;
+        end  //end firstpush
+
+        `full : begin
+            if (~ringcounter[6]) begin
+                nextstate = `full;
+            end
+            else begin
+                if (pushin) begin
+                    nextstate = `firstpush;
+                end
+                else begin
+                    nextstate = `empty;
+                end
+            end
+        end  //end full
+
+        `restart : begin
+            nextstate = `empty;
+        end  //end restart
+    endcase
+end
+/*==== END STATE MACHINE ====*/
+
+/*======== ROUND SETTER ========*/
+reg [4:0] round1, round2, round3;
+always @ (*) begin
+    if (state == `firstpush) begin
+        round1 = 5'd0;
+        round2 = 5'd1;
+        round3 = 5'd2;
+    end
+    else begin
+        case (ringcounter)
+            7'b0000001 : begin
+                round1 = 5'd3;
+                round2 = 5'd4;
+                round3 = 5'd5;
+            end
+            7'b0000010 : begin
+                round1 = 5'd6;
+                round2 = 5'd7;
+                round3 = 5'd8;
+            end
+            7'b0000100 : begin
+                round1 = 5'd9;
+                round2 = 5'd10;
+                round3 = 5'd11;
+            end
+            7'b0001000 : begin
+                round1 = 5'd12;
+                round2 = 5'd13;
+                round3 = 5'd14;
+            end
+            7'b0010000 : begin
+                round1 = 5'd15;
+                round2 = 5'd16;
+                round3 = 5'd17;
+            end
+            7'b0100000 : begin
+                round1 = 5'd18;
+                round2 = 5'd19;
+                round3 = 5'd20;
+            end
+            7'b1000000 : begin
+                round1 = 5'd21;
+                round2 = 5'd22;
+                round3 = 5'd23;
+            end
+            default : begin
+                round1 = 5'd0;
+                round2 = 5'd1;
+                round3 = 5'd2;
+            end
+        endcase
+    end
+end
+/*==== END ROUND SETTER ====*/
+
+/*======== DATAPATH ========*/
+assign pushout = ringcounter[6];
+assign data0 = (state == `firstpush) ? (din) : (recirculate);
+permLayer layer_one2 (data0, round1, data1);
+permLayer layer_two2 (data1, round2, data2);
+permLayer layer_thr2 (data2, round3, dout);
+/*==== END DATAPATH ====*/
+
+endmodule
+/*============ END PERM_LOGIC ============*/
 
 
 
-endmodule  //end permLogic
+/*============ LEVEL 2 PERM_LAYER ============*/
+module permLayer (din, roundin, dout);
+
+input [4:0][4:0][63:0]  din;
+input [4:0]             roundin;
+output [4:0][4:0][63:0] dout;
+
+wire [4:0][4:0][63:0]   din;
+wire [4:0]              roundin;
+wire [4:0][4:0][63:0]   dout;
+
+wire [4:0][4:0][63:0]   data0, data1, data2, data3;
+
+theta   theta0 (din, data0);
+//rho     rho0 (data0, data1);
+//pi      pi0 (data1, data2);
+//chi     chi0 (data2, data3);
+//iota    iota0 (data3, roundin, dout);
+assign dout = data0;
+
+
+endmodule
+/*============ END PERM_LAYER ============*/
+
+
+
+/*============ LEVEL 3 THETA ============*/
+module theta(theta_in,theta_out);
+
+input reg [4:0][4:0][63:0] theta_in;
+output reg [4:0] [4:0] [63:0] theta_out;
+
+reg [4:0][63:0] C,D;
+
+integer x,y,z;
+
+always @(*)
+begin
+  // STEP : 1
+  // For all pairs (x, z) such that 0 ≤ x < 5 and 0 ≤ z < w, let
+  // C[x, z] = A[x, 0, z] ⊕ A[x, 1, z] ⊕ A[x, 2, z] ⊕ A[x, 3, z] ⊕ A[x, 4, z].
+
+  // STEP : 2
+  //For all pairs (x, z) such that 0 ≤ x < 5 and 0 ≤ z < w let
+  //D[x, z] = C[(x-1) mod 5, z] ⊕ C[(x+1) mod 5, (z – 1) mod w].
+  for(x=0; x<5 ; x=x+1)
+  begin
+    for(z=0 ; z<64 ; z=z+1)
+    begin
+      C[x][z] = theta_in[x][0][z] ^ theta_in[x][1][z] ^ theta_in[x][2][z] ^ theta_in[x][3][z] ^ theta_in[x][4][z];
+      D[x][z] = C[modulo_operation_5(x-1,5)][z] ^ C[modulo_operation_5(x+1,5)] [modulo_operation_5(z-1,64)];
+    end 
+  end 
+
+  // STEP : 3
+  //For all triples (x, y, z) such that 0 ≤ x < 5, 0 ≤ y < 5, and 0 ≤ z < w, let
+  //A′[x, y, z] = A[x, y, z] ⊕ D[x, z].
+
+  for(x=0; x<5 ; x=x+1)
+  begin
+    for(y=0 ; y<5 ; y=y+1)
+    begin
+      for(z=0 ; z<64 ; z=z+1)
+      begin
+        theta_out[x][y][z] = theta_in[x][y][z] ^ D[x][z];
+      end 
+    end
+  end 
+end 
+
+function integer modulo_operation_5(input integer value,input integer modulo_value);
+
+if (value == modulo_value)
+begin
+  modulo_operation_5 = 0;
+end
+else if (value < 0)
+begin
+  modulo_operation_5 = modulo_value + value;
+end
+else begin
+  modulo_operation_5 = value;
+end 
+
+endfunction : modulo_operation_5 
+
+endmodule
+/*============ END THETA ============*/
+
+
